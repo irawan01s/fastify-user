349253278f119e1ad97b0da42afa6538533c61ce
irawan01s <irawan01s@gmail.com>
2021-06-10 15:29:32 +0700

    Update listen callback

--- index.js
+++ index.js
@@ -31,8 +31,10 @@ app.register(bankRoutes, { prefix: '/banks' })
 
 const start = async () => {
   try {
-    await app.listen(port)
-    console.log(`Server is running on port ${process.env.PORT}`)
+    await app.listen(port, '0.0.0.0', (err, address) => {
+      if (err) throw err
+      app.log.info(`server listening on ${address}`)
+  })
   } catch (err) {
     app.log.error(err)
     process.exit(1)
