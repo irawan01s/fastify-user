84488f9c3570924554d6c20915bc5371e5fcbfb6
irawan01s <irawan01s@gmail.com>
2021-06-10 10:45:10 +0700

    FIx port

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import './loadEnv.js'
+// import dotenv from 'dotenv'
+// dotenv.config()
 import fastify from 'fastify'
 import fastifyFormbody from 'fastify-formbody'
 import fastifyCors from 'fastify-cors'
@@ -6,7 +8,9 @@ import userRoutes from './routes/userRoutes.js'
 import bankRoutes from './routes/bankRoutes.js'
 
 const app = fastify({ logger: false })
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4001
+
+console.log(`Port ${process.env.PORT}`)
 
 app.register(fastifyCors, {
   origin: '*',
@@ -18,7 +22,7 @@ app.register(fastifyFormbody)
 app.get('/', (request, reply) => {
   reply.code(200).send({
     status: true,
-    message: 'Success'
+    message: 'Success Fastify'
   })
 })
 
@@ -28,6 +32,7 @@ app.register(bankRoutes, { prefix: '/banks' })
 const start = async () => {
   try {
     await app.listen(port)
+    console.log(`Server is running on port ${process.env.PORT}`)
   } catch (err) {
     app.log.error(err)
     process.exit(1)
